2978e0aa4f00d34e159424b343c7f2a37366c2e8
6ErickFE9 <141530808+6ErickFE9@users.noreply.github.com>
2024-04-04 17:08:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function calc() {
         };
         saida.appendChild(jogarNovamenteBtn);
 
-        let resposta = prompt("Deseja jogar novamente? (S/N)").toUpperCase();
+        let resposta = prompt("Caso deseje realizar outra operação pressione 'S',caso contrario pressione 'N' e verifique o resultado: ").toUpperCase();
         if (resposta !== 'S') {
             continuar = false;
         }
