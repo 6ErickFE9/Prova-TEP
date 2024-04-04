a4b54838de3a84b6672b139c6e99db1ad5d0b6bc
6ErickFE9 <141530808+6ErickFE9@users.noreply.github.com>
2024-04-04 16:56:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function calc() {
+    let continuar = true;
+
+    while (continuar) {
+        let n1 = Number(prompt('Primeiro valor:'))
+        let n2 = Number(prompt('Segundo valor:'))
+        let op = Number(prompt(`Valores informados: ${n1} e ${n2}. \nO que vamos fazer? \n[1] Somar \n[2] Subtrair \n[3] Multiplicar \n[4] Dividir`))
+
+        let saida = document.getElementById('saida')
+        saida.innerHTML = `<h2>Calculando...</h2>`
+        switch (op) {
+            case 1:
+                saida.innerHTML += `<p>${n1} + ${n2} = <strong>${n1+n2}</strong></p>`
+                break;
+            case 2:
+                saida.innerHTML += `<p>${n1} - ${n2} = <strong>${n1-n2}</strong></p>`
+                break;
+            case 3:
+                saida.innerHTML += `<p>${n1} x ${n2} = <strong>${n1*n2}</strong></p>`
+                break;
+            case 4:
+                saida.innerHTML += `<p>${n1} / ${n2} = <strong>${(n1/n2).toLocaleString('pt-BR')}</strong></p>`
+                break;
+            default:
+                saida.innerHTML += `<p>OPÇÃO INVÁLIDA! Você digitou ${n1} e ${n2}, mas não sei o que fazer com eles. </p>`
+                break;
+        }
+
+        let resposta = prompt("Deseja jogar novamente? (S/N)").toUpperCase();
+        if (resposta !== 'S') {
+            continuar = false;
+        }
+    }
+}
